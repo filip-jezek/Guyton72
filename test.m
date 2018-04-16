67af8752a8da066eb778cf812d023d7b1f20639c
Filip Ježek <filip.jezek@lf1.cuni.cz>
2018-04-16 19:54:06 +0200

    Export of raw values G72 values, maximal timestep 90 min

---
+++ test.m
@@ -0,0 +1,38 @@
+%% save data to csv ang get some plotting fun
+showG = false;
+csvfile = 'G72.csv';
+
+%% init
+ds = Simulink.SimulationData.Dataset(logsout);
+N = ds.numElements;
+%% get the data into my struct
+header{1} = 'Time';
+X(:, 1) = ds.get(1).Values.Time;
+for i = 2:N
+    header{i} = ds.get(i-1).Name;
+    X(:, i) = ds.get(i-1).Values.Data;
+    if showG
+        figure
+        plot(ds.get(i-1).Values.Time, ds.get(i-1).Values.Data);
+        title(ds.get(i-1).Name);
+    end
+end
+
+%%
+t1 = 20160;
+t2 = 30240;
+t3 = 40
+s1 = 
+clf;
+hold on;
+plot(X)
+
+%%
+textHeader = strjoin(header, ',');
+%write header to file
+fid = fopen(csvfile,'w'); 
+fprintf(fid,'%s\n',textHeader);
+fclose(fid);
+%write data to end of file
+dlmwrite(csvfile,X,'-append');
+
