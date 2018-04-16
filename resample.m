2f0d6b9bd5303d40b442086fb1146af0c805a136
Filip <jezekf@gmail.com>
2018-04-16 23:44:36 +0200

    fixed resample bug

--- resample.m
+++ resample.m
@@ -23,12 +23,17 @@ si2 = (ind1:ind2);
 si3 = (ind2:ind3);
 si4 = (ind3:N);
 
-do1 = interp1(time(si1), data(si1, :), 1:1440:t1);
-do2 = interp1(time(si2), data(si2, :), t1:360:t2);
-do3 = interp1(time(si3), data(si3, :), t2:360:t3);
-do4 = interp1(time(si4), data(si4, :), t2:10800:last);
+do1 = interp1(time, data, 1:1440:t1);
+do2 = interp1(time, data, t1:360:t2);
+do3 = interp1(time, data, t2:360:t3);
+do4 = interp1(time, data, t3:10800:last);
 
-timeo = [1:1440:t1, t1:360:t2, t2:360:t3, t2:10800:last];
+%do1 = interp1(time(si1), data(si1, :), 1:1440:t1);
+%do2 = interp1(time(si2), data(si2, :), t1:360:t2);
+%do3 = interp1(time(si3), data(si3, :), t2:360:t3);
+%do4 = interp1(time(si4), data(si4, :), t2:10800:last);
+
+timeo = [1:1440:t1, t1:360:t2, t2:360:t3, t3:10800:last];
 datao = [do1; do2; do3; do4];
 
 clf; hold on;
