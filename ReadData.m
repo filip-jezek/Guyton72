db1ca8c837d5d3b21376a8816041fce0c5dea063
Filip Ježek <filip.jezek@lf1.cuni.cz>
2018-04-25 09:53:44 +0200

    Resampled the datasets.

--- ReadData.m
+++ ReadData.m
@@ -3,14 +3,20 @@ showG = false;
 csvfile = 'G72.csv';
 
 %% init
-ds = Simulink.SimulationData.Dataset(logsout);
+% ds = Simulink.SimulationData.Dataset(logsout);
+file = 'g72 low salt non-oscillating.mat';
+% file = 'g72 lower salt oscillating.mat';
+% file = 'g72 lower salt oscillating.mat';
+raw_read = load(file);
+ds = raw_read.ds;
+% ds = raw_read;
 N = ds.numElements;
 %% get the data into my struct
 header{1} = 'Time';
 X(:, 1) = ds.get(1).Values.Time;
 for i = 2:N
     header{i} = ds.get(i-1).Name;
-%     disp(header{i});
+
     X(:, i) = ds.get(i-1).Values.Data;
     if showG
         figure
@@ -19,16 +25,10 @@ for i = 2:N
     end
 end
 
-%%
-t1 = 20160;
-t2 = 30240;
-t3 = 40
-s1 = 
-clf;
-hold on;
-plot(X)
+time = X(:, 1);
+data = X(:, 2:end);
 
-%%
+%% WRite data
 textHeader = strjoin(header, ',');
 %write header to file
 fid = fopen(csvfile,'w'); 
--- resample.m
+++ resample.m
@@ -1,8 +1,11 @@
 %% resample
-  file = 'G72.csv';
-  data_raw_read = importdata(file,',',2);
-  time = data_raw_read.data(:,1);
-  data = data_raw_read.data(:,2:end);
+
+%% load data from csv or use the ReadData script
+%   file = 'G72.csv';
+%   data_raw_read = importdata(file,',',2);
+%   time = data_raw_read.data(:,1);
+%   data = data_raw_read.data(:,2:end);
+%% expects time [X], data [X Y], header [Y]
   last = time(end);
   N = length(time);
   
@@ -28,21 +31,22 @@ do2 = interp1(time, data, t1:360:t2);
 do3 = interp1(time, data, t2:360:t3);
 do4 = interp1(time, data, t3:10800:last);
 
-%do1 = interp1(time(si1), data(si1, :), 1:1440:t1);
-%do2 = interp1(time(si2), data(si2, :), t1:360:t2);
-%do3 = interp1(time(si3), data(si3, :), t2:360:t3);
-%do4 = interp1(time(si4), data(si4, :), t2:10800:last);
-
 timeo = [1:1440:t1, t1:360:t2, t2:360:t3, t3:10800:last];
 datao = [do1; do2; do3; do4];
 
 clf; hold on;
 %plot(time(si1), data(si1, 1), '*')
-plot(timeo, datao(:, 1:3), '-')
-
+plot(timeo, datao(:, 1:3), '-*')
 
 %% write to file
-csvfile = 'G72resampled.csv';
+csvfile = 'G72resampled-Na0_04.csv';
+
+textHeader = strjoin(header, ',');
+%write header to file
+fid = fopen(csvfile,'w'); 
+fprintf(fid,'%s\n',textHeader);
+fclose(fid);
+
 
 datapack = [timeo', datao];
 
